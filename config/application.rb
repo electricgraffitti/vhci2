9ce330d489ca088a420e670a8e78167f26f240c7
Bob Hanson <bob@cube2media.com>
2012-01-30 20:05:12 -0700

    adding moonshine

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Vhci2
   class Application < Rails::Application
+  
+  # don't attempt to auto-require the moonshine manifests into the rails env
+  config.paths.app.manifests 'app/manifests', :eager_load => false"
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
