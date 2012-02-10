07ba8b10ea9d080a4cdfbbe1fb2407683c01987a
Larry Jo <larry@cube2media.com>
2012-02-09 17:10:51 -0800

    changed the nav to be more strong like verisk health changed bill review to prof review reduced main demo button size and text on button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,7 @@ module ApplicationHelper
   
   # banner get a demo button
   def demo
-     link_to "Get a Demo", more_info_path, :class => "button banner_button", :title => "Get a Demo"
+     link_to "Request Demo", more_info_path, :class => "button banner_button", :title => "Request Demo"
   end
   
 end
