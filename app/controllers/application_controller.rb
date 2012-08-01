98b2ed894b9a459c9ba40bf3c6736436909f3e66
Larry Finn <larry@cube2media.com>
2012-08-01 11:39:49 -0700

    add redirect to verisk health for HCI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  
+  
+  before_filter :redirect_to_vh
+  
   protect_from_forgery
+  
+  
+  private
+  
+  def redirect_to_vh
+    redirect_to "http://www.veriskhealth.com"
+  end
 end
